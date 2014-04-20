7f332c2ea87991c4f5385650dc1b47ffc1846ac8
Abhilash Raj <raj.abhilash1@gmail.com>
2014-04-20 10:15:13 +0530

    Initial commit

---
+++ indexer.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python
+# File Indexer
+# It searches for all file types, creates type specific directories
+# and moves the files inside those directories.
+#
+# Author: Abhilash Raj
+# Date: 20th April, 2014
+##################################################################
+
+import os
+import sys
+import glob
+import shutil
+
+dir = sys.argv[1]
+types = []
+video_types = ['mp4', 'mov', 'avi']
+for file in os.listdir(dir):
+    try:
+        if '.' in file:
+            extension = file.split('.')[-1]
+            if extension not in types:
+                    types.append(extension)
+    except IndexError:
+        pass
+
+for type in types:
+    try:
+        os.mkdir(dir + '/' + type)
+    except OSError:
+        pass
+    
+for file in os.listdir(dir):
+    if '.' in file:
+        extension = file.split('.')[-1]
+        shutil.move(dir + '/' + file, '{}/{}/'.format(dir, extension))
+print types
